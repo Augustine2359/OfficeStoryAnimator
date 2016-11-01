955c94d4efd9352fc83ffde46c13f6489acc3353
Augustine Cheong <augustine_cheong@hotmail.com>
2016-11-01 23:16:36 +0900

    Can add a person to the scene

---
+++ OfficeStoryAnimator/PersonView.swift
@@ -0,0 +1,21 @@
+//
+//  OfficeView.swift
+//  OfficeStoryAnimator
+//
+//  Created by Augustine Cheong on 2016/11/01.
+//  Copyright © 2016 Augustine Cheong. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class PersonView: NSImageView {
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        image = #imageLiteral(resourceName: "person.png")
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
