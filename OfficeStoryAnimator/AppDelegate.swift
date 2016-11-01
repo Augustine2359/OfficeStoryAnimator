f68cec800b1dd2a9607ebc607c391ae372d31893
Augustine Cheong <augustine_cheong@hotmail.com>
2016-11-01 21:48:14 +0900

    Initial Commit

---
+++ OfficeStoryAnimator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OfficeStoryAnimator
+//
+//  Created by Augustine Cheong on 2016/11/01.
+//  Copyright © 2016 Augustine Cheong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
