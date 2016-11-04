2ce32e80e189c87257858dd7bdeaf4dbef175051
Augustine Cheong <augustine_cheong@hotmail.com>
2016-11-05 01:02:23 +0900

    Create a group of tables

--- OfficeStoryAnimator/OfficeViewController.swift
+++ OfficeStoryAnimator/OfficeViewController.swift
@@ -10,6 +10,11 @@ import Foundation
 import Cocoa
 
 class OfficeViewController: NSViewController {
+    let tableGroup1x = 100
+    let tableGroup1y = 100
+    let standardTableWidth = 100
+    let standardTableHeight = 100
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,9 +23,33 @@ class OfficeViewController: NSViewController {
         scrollView.hasHorizontalScroller = true
         view.addSubview(scrollView)
 
-        let subview = NSView(frame: NSRect(x: 0, y: 0, width: 1500, height: 100))
-        let personView = PersonView(frame: NSRect(x: 1000, y: 0, width: 100, height: 100))
-        subview.addSubview(personView)
-        scrollView.documentView = subview
+        let tableGroup1Rects: [NSRect] = groupOfTableRectsFromXAndY(x: tableGroup1x, y: tableGroup1y)
+
+        let subView = NSView(frame: NSRect(x: 0, y: 0, width: 1500, height: 1500))
+        for rect in tableGroup1Rects {
+            let tableView = TableView(frame: rect)
+            subView.addSubview(tableView)
+        }
+
+//        let personView = PersonView(frame: NSRect(x: 1000, y: 0, width: 100, height: 100))
+//        subview.addSubview(personView)
+        scrollView.documentView = subView
+    }
+  
+    func groupOfTableRectsFromXAndY(x:Int, y:Int) -> [NSRect] {
+        var tableGroupRects: [NSRect] = []
+        tableGroupRects.append(NSRect(x: x, y: y, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x, y: y + standardTableHeight, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x, y: y + standardTableHeight*2, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x, y: y + standardTableHeight*3, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x, y: y + standardTableHeight*4, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x + standardTableWidth, y: y, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x + standardTableWidth, y: y + standardTableHeight, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x + standardTableWidth, y: y + standardTableHeight*2, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x + standardTableWidth, y: y + standardTableHeight*3, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x + standardTableWidth, y: y + standardTableHeight*4, width: standardTableWidth, height: standardTableHeight))
+        tableGroupRects.append(NSRect(x: x, y: y + standardTableHeight*5, width: 2*standardTableWidth, height: standardTableHeight))
+        
+        return tableGroupRects
     }
 }
