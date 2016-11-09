f1877f4058db796b79a129c7458f393d850d6f6d
Augustine Cheong <augustine_cheong@hotmail.com>
2016-11-10 01:17:19 +0900

    Added ghost and wait commands

--- OfficeStoryAnimator/OfficeViewController.swift
+++ OfficeStoryAnimator/OfficeViewController.swift
@@ -26,7 +26,8 @@ class OfficeViewController: NSViewController {
     var mePerson: PersonView?
     var deptHeadPerson: PersonView?
     var pmPerson: PersonView?
-
+    var ghostMePerson: PersonView?
+    
     var animationCommands: [Any] = []
     
     override func viewDidLoad() {
@@ -70,13 +71,25 @@ class OfficeViewController: NSViewController {
             subView.addSubview(personView)
         }
         
+        ghostMePerson = PersonView(frame: mePerson!.frame, direction: facingRightAngle, isVIP: true)
+        ghostMePerson?.alphaValue = 0
+        subView.addSubview(ghostMePerson!)
+        
         scrollView.documentView = subView
         
+        var waitDuration:Double = 5
+        animationCommands.append(waitDuration)
+        var textTuple = (speaker: mePerson!, text: "ENGINEER")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: pmPerson!, text: "PROJECT MANAGER")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: deptHeadPerson!, text: "DEPARTMENT HEAD")
+        animationCommands.append(textTuple)
         var rotateToPersonTuple = (turningPersonView: pmPerson!, targetPersonView: mePerson!)
         animationCommands.append(rotateToPersonTuple)
-        var textTuple = (speaker: pmPerson!, text: "Hey could you come here for a while?")
+        textTuple = (speaker: pmPerson!, text: "Hey could you come here for a while?")
         animationCommands.append(textTuple)
-        textTuple = (speaker: pmPerson!, text: "I need to consult you on something")
+        textTuple = (speaker: pmPerson!, text: "I need to consult you on something.")
         animationCommands.append(textTuple)
         var rotateToAngleTuple = (personView: pmPerson!, rotation: facingLeftAngle)
         animationCommands.append(rotateToAngleTuple)
@@ -92,27 +105,73 @@ class OfficeViewController: NSViewController {
         animationCommands.append(translateTuple)
         rotateToAngleTuple = (personView: mePerson!, rotation: facingDownAngle)
         animationCommands.append(rotateToAngleTuple)
-        translateTuple = (personView: mePerson!, translation: NSPoint(x: 0, y: -200))
+        translateTuple = (personView: mePerson!, translation: NSPoint(x: 0, y: -125))
         animationCommands.append(translateTuple)
         rotateToPersonTuple = (turningPersonView: mePerson!, targetPersonView: pmPerson!)
         animationCommands.append(rotateToPersonTuple)
-        textTuple = (speaker: mePerson!, text: "Blah blah")
+        textTuple = (speaker: mePerson!, text: "What is it?")
         animationCommands.append(textTuple)
-        textTuple = (speaker: pmPerson!, text: "Blah blah")
+        textTuple = (speaker: pmPerson!, text: " (consult) ")
         animationCommands.append(textTuple)
+        textTuple = (speaker: mePerson!, text: " (discuss) ")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: pmPerson!, text: " (consult) ")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: mePerson!, text: " (explain) ")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: pmPerson!, text: " (consult) ")
+        animationCommands.append(textTuple)
+        textTuple = (speaker: mePerson!, text: "Ok")
+        animationCommands.append(textTuple)
+        waitDuration = 2
+        animationCommands.append(waitDuration)
         textTuple = (speaker: pmPerson!, text: "By the way, next time don't walk behind the department head")
         animationCommands.append(textTuple)
+        textTuple = (speaker: pmPerson!, text: "Walk the other way around")
+        animationCommands.append(textTuple)
+
+        
+        translateTuple = (personView: ghostMePerson!, translation: NSPoint(x: -100, y: 0))
+        animationCommands.append(translateTuple)
+        rotateToAngleTuple = (personView: ghostMePerson!, rotation: facingDownAngle)
+        animationCommands.append(rotateToAngleTuple)
+        translateTuple = (personView: ghostMePerson!, translation: NSPoint(x: 0, y: -250))
+        animationCommands.append(translateTuple)
+        rotateToAngleTuple = (personView: ghostMePerson!, rotation: facingRightAngle)
+        animationCommands.append(rotateToAngleTuple)
+        translateTuple = (personView: ghostMePerson!, translation: NSPoint(x: 400, y: 0))
+        animationCommands.append(translateTuple)
+        rotateToAngleTuple = (personView: ghostMePerson!, rotation: facingUpAngle)
+        animationCommands.append(rotateToAngleTuple)
+        translateTuple = (personView: ghostMePerson!, translation: NSPoint(x: 0, y: 375))
+        animationCommands.append(translateTuple)
+        rotateToPersonTuple = (turningPersonView: ghostMePerson!, targetPersonView: pmPerson!)
+        animationCommands.append(rotateToPersonTuple)
+        waitDuration = 2
+        animationCommands.append(waitDuration)
+
+        textTuple = (speaker: pmPerson!, text: "Like that")
+        animationCommands.append(textTuple)
+
         rotateToPersonTuple = (turningPersonView: mePerson!, targetPersonView: deptHeadPerson!)
         animationCommands.append(rotateToPersonTuple)
+        waitDuration = 2
+        animationCommands.append(waitDuration)
         rotateToPersonTuple = (turningPersonView: mePerson!, targetPersonView: pmPerson!)
         animationCommands.append(rotateToPersonTuple)
+        waitDuration = 2
+        animationCommands.append(waitDuration)
         rotateToPersonTuple = (turningPersonView: mePerson!, targetPersonView: deptHeadPerson!)
         animationCommands.append(rotateToPersonTuple)
+        waitDuration = 2
+        animationCommands.append(waitDuration)
+        textTuple = (speaker: deptHeadPerson!, text: "?")
+        animationCommands.append(textTuple)
         rotateToPersonTuple = (turningPersonView: mePerson!, targetPersonView: pmPerson!)
         animationCommands.append(rotateToPersonTuple)
         textTuple = (speaker: mePerson!, text: "...")
         animationCommands.append(textTuple)
-        textTuple = (speaker: mePerson!, text: "Ok")
+        textTuple = (speaker: mePerson!, text: "...Ok")
         animationCommands.append(textTuple)
     }
 
@@ -124,10 +183,17 @@ class OfficeViewController: NSViewController {
         let superself = self
 
         let animationCommand = animationCommands.removeFirst()
+        
+        if let waitCommand = animationCommand as? Double {
+            Timer.scheduledTimer(withTimeInterval: waitCommand, repeats: false, block: { (timer) in
+                self.executeAnimationCommands()
+            })
+        }
+        
         if let textCommand = animationCommand as? (speaker: PersonView, text: String) {
             let textView = CustomTextView(speaker: textCommand.speaker, text: textCommand.text)
             view.addSubview(textView)
-            Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { (timer) in
+            Timer.scheduledTimer(withTimeInterval: 2.5, repeats: false, block: { (timer) in
                 textView.removeFromSuperview()
                 self.executeAnimationCommands()
             })
@@ -136,6 +202,13 @@ class OfficeViewController: NSViewController {
 
         if let rotateCommand = animationCommand as? (turningPersonView: PersonView, targetPersonView: PersonView) {
             Swift.print("rotating to person")
+            if (rotateCommand.turningPersonView.isEqual(ghostMePerson)) {
+                ghostMePerson?.alphaValue = 0.5
+            }
+            else {
+                ghostMePerson?.alphaValue = 0
+            }
+            
             rotateCommand.turningPersonView.turnTo(otherPerson: rotateCommand.targetPersonView, completionHandler: { 
                 self.executeAnimationCommands()
             })
@@ -144,6 +217,13 @@ class OfficeViewController: NSViewController {
 
         if let translateCommand = animationCommand as? (personView: PersonView, translation: NSPoint) {
             Swift.print("moving")
+            if (translateCommand.personView.isEqual(ghostMePerson)) {
+                ghostMePerson?.alphaValue = 0.5
+            }
+            else {
+                ghostMePerson?.alphaValue = 0
+            }
+
             let animation = NSViewAnimation(duration: 0.5, animationCurve: NSAnimationCurve.linear)
             var newFrame = translateCommand.personView.frame
             newFrame.origin.x += translateCommand.translation.x
@@ -159,6 +239,14 @@ class OfficeViewController: NSViewController {
         
         if let rotateCommand = animationCommand as? (personView: PersonView, rotation: CGFloat) {
             Swift.print("looking at something")
+            if (rotateCommand.personView.isEqual(ghostMePerson)) {
+                ghostMePerson?.alphaValue = 0.5
+            }
+            else {
+                ghostMePerson?.alphaValue = 0
+            }
+
+            
             rotateCommand.personView.turnTo(angle: rotateCommand.rotation, completionHandler: {
                 self.executeAnimationCommands()
             })
@@ -229,9 +317,6 @@ class OfficeViewController: NSViewController {
         
         personViewRect.origin.y -= CGFloat(standardPersonHeight*2)
         personViewRect.origin.x += CGFloat(standardPersonWidth + standardTableWidth/2)
-        personViews.append(PersonView(frame: personViewRect, direction: facingLeftAngle, isVIP: false))
-        
-        personViewRect.origin.y -= CGFloat(standardPersonHeight)
         if (hasVIPs) {
             pmPerson = PersonView(frame: personViewRect, direction: facingLeftAngle, isVIP: true)
             personViews.append(pmPerson!)
@@ -239,6 +324,9 @@ class OfficeViewController: NSViewController {
         else {
             personViews.append(PersonView(frame: personViewRect, direction: facingLeftAngle, isVIP: false))
         }
+        
+        personViewRect.origin.y -= CGFloat(standardPersonHeight)
+        personViews.append(PersonView(frame: personViewRect, direction: facingLeftAngle, isVIP: false))
 
         personViewRect.origin.y -= CGFloat(standardPersonHeight)
         personViews.append(PersonView(frame: personViewRect, direction: facingLeftAngle, isVIP: false))
